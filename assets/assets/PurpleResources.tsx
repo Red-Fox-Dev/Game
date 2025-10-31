3a3b230c0773755a77858a17aa20e1bd0314a32e
NewmoodLev <attakorn.phth@gmail.com>
2025-10-31 09:50:59 +0700

    asset game

---
+++ assets/assets/PurpleResources.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="PurpleResources" tilewidth="32" tileheight="16" tilecount="5" columns="1">
+ <image source="../../Assest Game/MiniWorldSprites/Buildings/Purple/PurpleResources.png" width="48" height="80"/>
+</tileset>
---
+++ assets/assets/WinterTrees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="WinterTrees" tilewidth="32" tileheight="16" tilecount="8" columns="2">
+ <image source="./MiniWorldSprites/Nature/WinterTrees.png" width="64" height="64"/>
+</tileset>
